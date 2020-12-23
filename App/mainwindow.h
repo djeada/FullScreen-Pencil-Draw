215d979a63f6ed11ecf662f9fdd9ef72236ea7a5
djeada <addjellouli1@gmail.com>
2020-12-24 00:36:42 +0100

    added support for saving svg files

--- App/mainwindow.h
+++ App/mainwindow.h
@@ -16,8 +16,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionSave_to_svg_triggered();
+
 private:
     Ui::MainWindow *ui;
     PaintScene* scene;
+    void saveSceneToSvg(const QString &filename);
 };
 #endif // MAINWINDOW_H
