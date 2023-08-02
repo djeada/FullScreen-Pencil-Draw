f523ee06771b1e4864f3a403aa4bd1b152667a6d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-08-02 20:39:59 +0200

    Update and rename App/mainwindow.cpp to App/windows/main_window.cpp

---
+++ App/windows/main_window.cpp
@@ -0,0 +1,14 @@
+#include "main_window.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), canvas(new Canvas(this))
+{
+    this->setCentralWidget(canvas);
+    this->setWindowTitle("Paint Application");
+    this->resize(800, 600); // Set to desired initial window size
+}
+
+MainWindow::~MainWindow()
+{
+    delete canvas;
+}
